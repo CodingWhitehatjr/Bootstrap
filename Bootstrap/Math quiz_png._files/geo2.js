2de4ec27662ef5e0b2ed7b7222ee4ad43c272113
CodingWhitehatjr <83097328+CodingWhitehatjr@users.noreply.github.com>
2021-05-07 16:57:07 -0400

    Add files via upload

---
+++ Bootstrap/Math quiz_png._files/geo2.js
@@ -0,0 +1,16 @@
+(function(){
+  window['optimizely'] = window['optimizely'] || [];
+  window['optimizely'].push(['activateGeoDelayedExperiments', {
+    'location':{
+      'city': "FREEHOLD",
+      'continent': "NA",
+      'country': "US",
+      'region': "NJ"
+    },
+    'ip':"71.59.74.68"
+  }]);
+})
+//
+()
+
+;
